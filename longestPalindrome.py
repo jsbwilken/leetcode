e00232334a697d48a55d65e42a0dd6155c1d6de1
Johannes Wilken <7515971+jsbwilken@users.noreply.github.com>
2023-08-14 20:31:10 +0200

    added longestPalindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,31 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longest_palindrome = 0
-        palindrome_idx = 0
-
-        mat = [[0 for _ in range(__ + 1)] for __ in range(len(s) + 1)]
+        max_length = -1
+        palindrome = ""
 
         for i in range(len(s)):
-            for n in range(i + 1):
-                if s[i] == s[-1-n]:
-                    if i - 1 >= 0 and n - 1 >= 0:
-                        mat[i][n] = 1 + mat[i - 1][n - 1]
-                    else:
-                        mat[i][n] = 1
+            left = i
+            right = i
 
-                    if mat[i][n] > longest_palindrome:
-                        longest_palindrome = mat[i][n]
-                        palindrome_idx = i
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    if (right - left + 1) > max_length:
+                        palindrome = s[left:right+1]
+                        max_length = right - left + 1
 
-        palindrome = ""
-        while longest_palindrome > 0:
-            palindrome += s[palindrome_idx]
-            palindrome_idx -= 1
-            longest_palindrome -= 1
+                    left -= 1
+                    right += 1
+                else:
+                    break
+
+            left = i
+            right = i + 1
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    if (right - left + 1) > max_length:
+                        palindrome = s[left:right+1]
+                        max_length = right - left + 1
+
+                    left -= 1
+                    right += 1
+                else:
+                    break
 
         return palindrome
 
 
+assert Solution().longestPalindrome("ccc") in ["ccc"]
+assert Solution().longestPalindrome("a") in ["a"]
 assert Solution().longestPalindrome("ccd") in ["cc"]
 assert Solution().longestPalindrome("racecar") in ["racecar"]
 assert Solution().longestPalindrome("babad") in ["bab", "aba"]
